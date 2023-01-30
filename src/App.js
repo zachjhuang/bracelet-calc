8580d35b372b906f28376a9f7d761ee02308a766
Zach Huang <zhuo-nanhuang2024@u.northwestern.edu>
2023-01-30 15:48:13 -0600

    Set icon and website title, WIP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a bracelet calculator for Lost Ark (or at least it will be)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Pausechamp
+        </p>
       </header>
     </div>
   );
